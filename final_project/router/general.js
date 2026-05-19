941694c0b40cddb86d9df17b8638fc084a41e958
Ahlangene <ndana.ahlangene@gmail.com>
2026-05-19 05:24:15 -0400

    Books routing modified

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,52 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books[1]),null,4);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+  if (isbn<=10 && isbn>0){  
+  return req.send(books[isbn]);
+  }
+  return req.send("ISBN for the book not found.");
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author=req.params.author;
+  books.forEach(book => {
+    if (author==book.author){
+        return req.send(JSON.stringify(book));
+    }
+  });
+  return req.send("The author is not found");
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title=req.params.title;
+    books.forEach(book => {
+      if (title==book.title){
+          return req.send(JSON.stringify(book));
+      }
+    });
+    return req.send("Book title is not found");
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn=req.params.isbn;
+    
+    if (isbn<=10 && isbn>0){  
+        return req.send(books[isbn].reviews);    
+    }
+        return req.send("Review for the book not found.");
+      
 });
 
 module.exports.general = public_users;
